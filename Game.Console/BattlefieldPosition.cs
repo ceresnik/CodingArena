a0384314fb1686b0b6df3b0c9ebfe43a1f8c1282
Peter <adronius@gmail.com>
2019-01-14 20:58:05 +0100

    Implemented BattlefieldPosition

---
+++ Game.Console/BattlefieldPosition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Game.Console
+{
+    internal class BattlefieldPosition
+    {
+        private int X { get; }
+        private int Y { get; }
+        private Battlefield Battlefield { get; }
+
+        public BattlefieldPosition(int x, int y, Battlefield battlefield)
+        {
+            X = x;
+            Y = y;
+            Battlefield = battlefield;
+        }
+
+        public double DistanceTo(BattlefieldPosition position) => 
+            Math.Sqrt(Math.Pow(position.X - X, 2) + Math.Pow(position.Y - Y, 2));
+
+        public double DistanceToBorder() => 
+            Math.Min(
+                Math.Min(X, Battlefield.Size.Width - X), 
+                Math.Min(Y, Battlefield.Size.Height - Y));
+    }
+}
--- Game.Console/IBot.cs
+++ Game.Console/IBot.cs
@@ -2,5 +2,14 @@
 {
     internal interface IBot
     {
+        int MaxHealthPoints { get; }
+        int HealthPoints { get; }
+        double HealthPercentage { get; }
+
+        int MaxShieldPoints { get; }
+        int ShieldPoints { get; }
+        double ShieldPercentage { get; }
+
+        BattlefieldPosition Position { get; }
     }
 }
