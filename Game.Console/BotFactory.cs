745cd297ca4fb89e928f4d32afd1061b53a7a00c
Peter <adronius@gmail.com>
2019-01-15 20:43:10 +0100

    adapted test bot factory

--- Game.Console/BotFactory.cs
+++ Game.Console/BotFactory.cs
@@ -1,51 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Game.Console
 {
     internal class BotFactory
     {
-        public virtual ICollection<IBot> CreateBots() => new Collection<IBot>();
+        public virtual ICollection<IBot> CreateBots(Battlefield battlefield) => 
+            new Collection<IBot>();
     }
 
     internal class TestBotFactory : BotFactory
     {
-        public override ICollection<IBot> CreateBots()
+        public override ICollection<IBot> CreateBots(Battlefield battlefield)
         {
+            var random = new Random((int) DateTime.Now.Ticks);
             return new List<IBot>
             {
-                new TestBotA(),
-                new TestBotB(),
-                new TestBotC(),
+                new SeekAndDestroyBot("BotA", GenerateRandomPosition(battlefield, random)),
+                new SeekAndDestroyBot("BotB", GenerateRandomPosition(battlefield, random)),
+                new SeekAndDestroyBot("BotC", GenerateRandomPosition(battlefield, random)),
             };
         }
-    }
 
-    internal class TestBotA : SimpleBot
-    {
-        public TestBotA() : base("TestBotA")
+        private static BattlefieldPosition GenerateRandomPosition(Battlefield battlefield, Random random)
         {
+            var x = random.Next(battlefield.Size.Width);
+            var y = random.Next(battlefield.Size.Height);
+            return new BattlefieldPosition(x, y, battlefield);
         }
     }
 
-    internal class SimpleBot : IBot
+    internal class SeekAndDestroyBot : IBot
     {
-        public SimpleBot(string name)
+        public SeekAndDestroyBot(string name, BattlefieldPosition position)
         {
             Name = name;
             MaxHealthPoints = 1000;
             HealthPoints = MaxHealthPoints;
+            MaxShieldPoints = 1000;
+            ShieldPoints = MaxShieldPoints;
+            Position = position;
         }
 
         public string Name { get; }
+
         public int MaxHealthPoints { get; }
+
         public int HealthPoints { get; }
 
         public double HealthPercentage => HealthPoints * 100 / (double) MaxHealthPoints;
 
         public int MaxShieldPoints { get; }
+
         public int ShieldPoints { get; }
-        public double ShieldPercentage { get; }
+
+        public double ShieldPercentage => ShieldPoints * 100 / (double)MaxShieldPoints;
+
         public BattlefieldPosition Position { get; }
     }
 }
