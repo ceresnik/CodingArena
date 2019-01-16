8f2ea791c7d6419c808d94741b20b391fe55a91e
Peter <adronius@gmail.com>
2019-01-16 19:56:17 +0100

    Revert "adapted test bot factory"
    This reverts commit 745cd297ca4fb89e928f4d32afd1061b53a7a00c.

--- Game.Console/BotFactory.cs
+++ Game.Console/BotFactory.cs
@@ -1,62 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Game.Console
 {
     internal class BotFactory
     {
-        public virtual ICollection<IBot> CreateBots(Battlefield battlefield) => 
-            new Collection<IBot>();
+        public virtual ICollection<IBot> CreateBots() => new Collection<IBot>();
     }
 
     internal class TestBotFactory : BotFactory
     {
-        public override ICollection<IBot> CreateBots(Battlefield battlefield)
+        public override ICollection<IBot> CreateBots()
         {
-            var random = new Random((int) DateTime.Now.Ticks);
             return new List<IBot>
             {
-                new SeekAndDestroyBot("BotA", GenerateRandomPosition(battlefield, random)),
-                new SeekAndDestroyBot("BotB", GenerateRandomPosition(battlefield, random)),
-                new SeekAndDestroyBot("BotC", GenerateRandomPosition(battlefield, random)),
+                new TestBotA(),
+                new TestBotB(),
+                new TestBotC(),
             };
-        }
-
-        private static BattlefieldPosition GenerateRandomPosition(Battlefield battlefield, Random random)
-        {
-            var x = random.Next(battlefield.Size.Width);
-            var y = random.Next(battlefield.Size.Height);
-            return new BattlefieldPosition(x, y, battlefield);
         }
     }
 
-    internal class SeekAndDestroyBot : IBot
+    internal class TestBotA : SimpleBot
     {
-        public SeekAndDestroyBot(string name, BattlefieldPosition position)
+        public TestBotA() : base("TestBotA")
+        {
+        }
+    }
+
+    internal class SimpleBot : IBot
+    {
+        public SimpleBot(string name)
         {
             Name = name;
             MaxHealthPoints = 1000;
             HealthPoints = MaxHealthPoints;
-            MaxShieldPoints = 1000;
-            ShieldPoints = MaxShieldPoints;
-            Position = position;
         }
 
         public string Name { get; }
-
         public int MaxHealthPoints { get; }
-
         public int HealthPoints { get; }
 
         public double HealthPercentage => HealthPoints * 100 / (double) MaxHealthPoints;
 
         public int MaxShieldPoints { get; }
-
         public int ShieldPoints { get; }
-
-        public double ShieldPercentage => ShieldPoints * 100 / (double)MaxShieldPoints;
-
+        public double ShieldPercentage { get; }
         public BattlefieldPosition Position { get; }
     }
 }
