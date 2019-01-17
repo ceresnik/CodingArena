93cc49383b7cd1df9abc04f748c56b2c33b255ce
Peter <adronius@gmail.com>
2019-01-17 23:05:05 +0100

    moved to partial files

---
+++ CodingArena.Player/TurnActions/Attack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodingArena.Player.TurnActions
+{
+    public static partial class TurnAction
+    {
+        public sealed partial class Attack
+        {
+            private Attack(IEnemy enemy)
+            {
+                Enemy = enemy ?? throw new ArgumentNullException(nameof(enemy));
+            }
+
+            public static Attack Target(IEnemy enemy) => new Attack(enemy);
+            public IEnemy Enemy { get; }
+        }
+    }
+}
