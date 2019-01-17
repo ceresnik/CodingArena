53b50d01da362243b7302da42c10da2303013572
Peter <adronius@gmail.com>
2019-01-17 22:57:14 +0100

    reformatted Move

--- CodingArena.Player/TurnAction.cs
+++ CodingArena.Player/TurnAction.cs
@@ -29,38 +29,25 @@ namespace CodingArena.Player
                 int difX = to.X - from.X;
                 int difY = to.Y - from.Y;
 
-                if (difX == 0 && difY == 0)
-                    return new Move(Direction.None);
+                if (difX == 0 && difY == 0) return new Move(Direction.None);
 
-                if (difX > 0 && difY == 0)
-                    return East();
-                if (difX < 0 && difY == 0)
-                    return West();
+                if (difX > 0 && difY == 0) return East();
+                if (difX < 0 && difY == 0) return West();
 
-                if (difX == 0 && difY > 0)
-                    return South();
-                if (difX == 0 && difY < 0)
-                    return North();
+                if (difX == 0 && difY > 0) return South();
+                if (difX == 0 && difY < 0) return North();
 
-                if (difX > 0 && difY > 0 && difX > difY)
-                    return East();
-                if (difX > 0 && difY > 0 && difX < difY)
-                    return South();
+                if (difX > 0 && difY > 0 && difX > difY) return East();
+                if (difX > 0 && difY > 0 && difX < difY) return South();
 
-                if (difX < 0 && difY > 0 && Math.Abs(difX) > difY)
-                    return West();
-                if (difX < 0 && difY > 0 && Math.Abs(difX) < difY)
-                    return South();
+                if (difX < 0 && difY > 0 && Math.Abs(difX) > difY) return West();
+                if (difX < 0 && difY > 0 && Math.Abs(difX) < difY) return South();
 
-                if (difX < 0 && difY < 0 && Math.Abs(difX) > Math.Abs(difY))
-                    return West();
-                if (difX < 0 && difY < 0 && Math.Abs(difX) < Math.Abs(difY))
-                    return North();
+                if (difX < 0 && difY < 0 && Math.Abs(difX) > Math.Abs(difY)) return West();
+                if (difX < 0 && difY < 0 && Math.Abs(difX) < Math.Abs(difY)) return North();
 
-                if (difX > 0 && difY < 0 && difX > Math.Abs(difY))
-                    return East();
-                if (difX > 0 && difY < 0 && difX < Math.Abs(difY))
-                    return North();
+                if (difX > 0 && difY < 0 && difX > Math.Abs(difY)) return East();
+                if (difX > 0 && difY < 0 && difX < Math.Abs(difY)) return North();
 
                 throw new NotSupportedException();
             }
