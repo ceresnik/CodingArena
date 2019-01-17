5a73b98cf0ff5ee9f482c735af919c56213c7730
Peter <adronius@gmail.com>
2019-01-17 21:44:24 +0100

    Implemented IEnemy interface.

---
+++ CodingArena.Game/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CodingArena.Player;
+
+namespace CodingArena.Game
+{
+    internal class Enemy : Robot, IEnemy
+    {
+        public Enemy(string name, int maxHP, int hp, int maxSP, int sp) : base(maxHP, hp, maxSP, sp)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException(
+                    "Value cannot be null or whitespace.", nameof(name));
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
