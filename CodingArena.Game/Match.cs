eb81c3d2f78980e33f21f1727d7d20ab0b3c18c6
Peter Milovcik <peter.milovcik@siemens-healthineers.com>
2019-01-31 15:58:01 +0100

    Remove unused using

--- CodingArena.Game/Match.cs
+++ CodingArena.Game/Match.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Threading.Tasks;
-using CodingArena.Player.Battlefield;
 
 namespace CodingArena.Game
 {
