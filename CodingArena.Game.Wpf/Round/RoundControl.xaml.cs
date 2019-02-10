761f1938a4b2085dd9dbf4bd46979ae8de9b8932
Peter Milovcik <peter.milovcik@siemens-healthineers.com>
2019-02-10 23:07:23 +0100

    Extract MatchControl and RoundControl to own user controls

---
+++ CodingArena.Game.Wpf/Round/RoundControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CodingArena.Game.Wpf.Round
+{
+    /// <summary>
+    /// Interaction logic for RoundControl.xaml
+    /// </summary>
+    public partial class RoundControl : UserControl
+    {
+        public RoundControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
