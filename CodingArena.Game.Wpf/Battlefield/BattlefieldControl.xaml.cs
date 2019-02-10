c3262e997b3958492687d20397a67ffa2f41edb1
Peter Milovcik <peter.milovcik@siemens-healthineers.com>
2019-02-10 22:37:18 +0100

    Extract battlefield into it's own user control

---
+++ CodingArena.Game.Wpf/Battlefield/BattlefieldControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodingArena.Game.Wpf.Battlefield
+{
+    /// <summary>
+    /// Interaction logic for BattlefieldControl.xaml
+    /// </summary>
+    public partial class BattlefieldControl : UserControl
+    {
+        public BattlefieldControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
