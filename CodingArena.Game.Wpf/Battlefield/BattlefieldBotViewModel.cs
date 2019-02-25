0c297f6721e337cb8a40c14e0b4ed1b2abdf5500
Peter Milovcik <peter.milovcik@siemens-healthineers.com>
2019-02-25 20:17:31 +0100

    Add progress bars for HP, SP, EP in BattlefieldBotControl

--- CodingArena.Game.Wpf/Battlefield/BattlefieldBotViewModel.cs
+++ CodingArena.Game.Wpf/Battlefield/BattlefieldBotViewModel.cs
@@ -1,5 +1,6 @@
 using CodingArena.Game.Entities;
 using CodingArena.Game.Wpf.Common;
+using System.Windows;
 
 namespace CodingArena.Game.Wpf.Battlefield
 {
@@ -9,6 +10,13 @@ namespace CodingArena.Game.Wpf.Battlefield
         private int myX;
         private int myY;
         private string myImageSource;
+        private int myHP;
+        private int myMaxHP;
+        private int myMaxSP;
+        private int mySP;
+        private int myMaxEP;
+        private int myEP;
+        private Visibility myProgressBarVisibility;
 
         public BattlefieldBotViewModel(IBattleBot battleBot)
         {
@@ -59,12 +67,94 @@ namespace CodingArena.Game.Wpf.Battlefield
             }
         }
 
+        public int HP
+        {
+            get => myHP;
+            set
+            {
+                if (value == myHP) return;
+                myHP = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int MaxHP
+        {
+            get => myMaxHP;
+            set
+            {
+                if (value == myMaxHP) return;
+                myMaxHP = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int MaxSP
+        {
+            get => myMaxSP;
+            set
+            {
+                if (value == myMaxSP) return;
+                myMaxSP = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SP
+        {
+            get => mySP;
+            set
+            {
+                if (value == mySP) return;
+                mySP = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int MaxEP
+        {
+            get => myMaxEP;
+            set
+            {
+                if (value == myMaxEP) return;
+                myMaxEP = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int EP
+        {
+            get => myEP;
+            set
+            {
+                if (value == myEP) return;
+                myEP = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Visibility ProgressBarVisibility
+        {
+            get => myProgressBarVisibility;
+            set
+            {
+                if (value == myProgressBarVisibility) return;
+                myProgressBarVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
         public void UpdateFrom(IBattleBot bot)
         {
             BotName = bot.Name;
             X = bot.Position.X * 14;
             Y = 700 - bot.Position.Y * 14 - 14;
             ImageSource = bot.HP > 0 ? "robot.png" : "scrap.png";
+            MaxHP = bot.MaxHP;
+            HP = bot.HP;
+            MaxSP = bot.MaxSP;
+            SP = bot.SP;
+            ProgressBarVisibility = HP > 0 ? Visibility.Visible : Visibility.Hidden;
         }
     }
 }
